4371881b89caa879fa19be6032a586425aabe34c
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-03-27 13:27:51 -0500

    Variables de ambiente por defecto para BD

--- src/models/db.py
+++ src/models/db.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST')
-db_port = os.getenv('DB_PORT')
-db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER', 'laad')
+db_password = os.getenv('DB_PASSWORD', 'laad')
+db_host = os.getenv('DB_HOST', 'localhost')
+db_port = os.getenv('DB_PORT', '5432')
+db_name = os.getenv('DB_NAME', 'sport_app_db')
 
 engine = create_engine(
     f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
