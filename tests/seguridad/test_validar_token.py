78830fed11703a1631acab58e1777569f02b13f4
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-03-27 15:59:07 -0500

    ajuste pruebas

--- tests/seguridad/test_validar_token.py
+++ tests/seguridad/test_validar_token.py
@@ -8,7 +8,7 @@ fake = Faker()
 
 class TestValidarToken():
 
-    def test_vlidar_token_valido(self):
+    def test_validar_token_valido(self):
         with app.test_client() as test_client:
             email_random = fake.email()
             body_get_token = {"email": email_random}
@@ -21,8 +21,20 @@ class TestValidarToken():
                 '/autorizador/seguridad/validar-token', json=body)
             response_json = json.loads(resp.data)
 
-            assert response.status_code == 200
+            assert resp.status_code == 200
             assert 'token_valido' in response_json
             assert response_json['token_valido'] == True
             assert 'email' in response_json
             assert response_json['email'] == email_random
+
+    def test_validar_token_invalido(self):
+        with app.test_client() as test_client:
+            body = {"token": "token_invalido"}
+
+            response = test_client.post(
+                '/autorizador/seguridad/validar-token', json=body)
+
+            response_json = json.loads(response.data)
+            assert response.status_code == 200
+            assert 'token_valido' in response_json
+            assert response_json['token_valido'] == False
