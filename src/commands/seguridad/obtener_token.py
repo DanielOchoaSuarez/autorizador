d4cbc24f3f926e2ae3e3df3d1d800fc3769ebc76
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-04-23 23:22:39 -0500

    Nuevos campos opcionales
    subscripcion y tipo_usuario

--- src/commands/seguridad/obtener_token.py
+++ src/commands/seguridad/obtener_token.py
@@ -9,11 +9,24 @@ logger = logging.getLogger(__name__)
 
 
 class ObtenerToken(BaseCommand):
-    def __init__(self, email: str):
-        self.email = email
+    def __init__(self, **info):
+        self.email = info['email']
+        self.subscripcion = info['subscripcion']
+        self.tipo_usuario = info['tipo_usuario']
 
     def execute(self):
-        token = jwt.encode({'email': self.email},
+        informacion_token = {
+            'email': self.email,
+        }
+
+        if self.subscripcion:
+            informacion_token['subscripcion'] = self.subscripcion
+
+        if self.tipo_usuario:
+            informacion_token['tipo_usuario'] = self.tipo_usuario
+
+        token = jwt.encode(informacion_token,
                            jwt_secret_key, algorithm='HS256')
+
         logger.info(f'Token generado para {self.email}')
         return token
