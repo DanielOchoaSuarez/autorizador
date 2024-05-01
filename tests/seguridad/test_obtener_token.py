6ec0f82cd026c94c6ff7c29d7105d87fd539886c
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-05-01 17:09:40 -0500

    Ajuste recursos y sonar

--- tests/seguridad/test_obtener_token.py
+++ tests/seguridad/test_obtener_token.py
@@ -8,9 +8,24 @@ fake = Faker()
 
 class TestObtenerToken():
 
-    def test_health(self):
+    def test_obtener_token_email(self):
         with app.test_client() as test_client:
             body = {"email": fake.email()}
+
+            response = test_client.post(
+                '/autorizador/seguridad/generar-token', json=body)
+            response_json = json.loads(response.data)
+
+            assert response.status_code == 200
+            assert 'token' in response_json
+
+    def test_obtener_token_todos_los_campos(self):
+        with app.test_client() as test_client:
+            body = {
+                "email": fake.email(),
+                "subscripcion": 'Premium',
+                "tipo_usuario": 'deportista',
+            }
 
             response = test_client.post(
                 '/autorizador/seguridad/generar-token', json=body)
